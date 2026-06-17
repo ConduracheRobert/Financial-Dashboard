80b2da39fcc150e73439ebf609a61d93c4d019a4
ConduracheRobert <condurache.robert19@gmail.com>
2026-06-17 11:25:12 +0300

    Inlocuire analiza Gemini cu analiza algoritmica proprie + actualizare lucrare

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.e2g7jph4kd4"
+    "revision": "0.9nm7olpbrfs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
